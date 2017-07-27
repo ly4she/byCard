5c58cf3336cb8ae52440b55f2bd0ed373d236e1f
ly4she <ly4she@gmail.com>
2017-07-27 18:30:45 +0300

    promise2

--- js/common.min.js
+++ js/common.min.js
@@ -1,3 +1,7 @@
+	if (!window.Promise) {
+  window.Promise = Promise;
+}
+
 	fetch('https://gist.githubusercontent.com/Kirit0s/53a9820793c1cdf22a9bfb1b71dbc84d/raw/14c5ee2ff6f9fef5e5dbd19b93c28ec45be32791/db')
 	.then(response => response.json())
 	.then(data => {
