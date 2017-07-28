9e68345c85ae58b31ced393dbb3ec58bda1496ac
ly4she <ly4she@gmail.com>
2017-07-28 11:39:18 +0300

    1139

--- js/content.js
+++ js/content.js
@@ -1,35 +1,24 @@
-	if (!window.Promise) {
-  window.Promise = Promise;
-}
-
-	fetch('https://gist.githubusercontent.com/Kirit0s/53a9820793c1cdf22a9bfb1b71dbc84d/raw/14c5ee2ff6f9fef5e5dbd19b93c28ec45be32791/db')
-	.then(response => response.json())
-	.then(data => {
-		data.forEach((element, i) => {
-			let blockToAdd = `<div class="films__item">
-				<div class="films__date">
-					<span>${element.dateStart}</span>
-				</div>
-				<div class="films__image">
-					<div class="films__duration">
-						<span>${element.durationMin} мин.</span>
-					</div>
-					<div class="films__ageLimit">
-						<span>${element.ageLimit}</span>
-					</div>
-					<img src="${element.image}" alt="">
-				</div>
-				<div class="films__sbscribe">
-					<div class="films__button">
-						<span>подписаться</span>
-					</div>
-				</div>
-			</div>`;
-			
-			if(i != 5) {
-				console.log(i);
-				document.querySelector('.films').innerHTML += blockToAdd;
-			}
-		})
+fetch('https://gist.githubusercontent.com/Kirit0s/53a9820793c1cdf22a9bfb1b71dbc84d/raw/14c5ee2ff6f9fef5e5dbd19b93c28ec45be32791/db')
+.then(response => response.json())
+.then(data => {
+	data.forEach((element) => {
+		let blockToAdd = `<div class="films__item">
+			<div class="films__date">
+				${element.dateStart}
+			</div>
+			<div class="films__title">
+				${element.title}
+			</div>
+			<div class="films__image">
+				<img src="${element.image}" alt="">
+			</div>
+			<div class="films__subscribe">
+				<button class="films__button">подписаться</button>
+			</div>
+		</div>`;
+		var i = 1;
+		document.querySelector('.films').insertAdjacentHTML('beforeend', blockToAdd);
+		
 	})
+})
 
